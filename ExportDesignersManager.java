527f81ceb1ab3d3004d74aece02e27fbc6ea22df
Mammadov Elmar <67194981+elmarMamedov485@users.noreply.github.com>
2023-01-04 13:46:00 +0400

    The exportDesigners was changed so that it now add only unique designers without repetition

--- ExportDesignersManager.java
+++ ExportDesignersManager.java
@@ -1,10 +1,29 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ExportDesignersManager {
     public static void exportDesigners(List<Item> list){
+        Set<String> setDesigners = new HashSet<>();
+
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).getDesigner().charAt(0) >= 48  && list.get(i).getDesigner().charAt(0) <= 57){
+                continue;
+            }
+            if(list.get(i).getDesigner().contains("/")){
+                String[] designers = list.get(i).getDesigner().split("/");
+                for(int j = 0; j < designers.length; j++){
+                    setDesigners.add(designers[j]);
+                }
+            }else{
+                setDesigners.add(list.get(i).getDesigner());
+            }
+        }
+
         File theDir = new File("src/Designers");
         if (!theDir.exists()){
             theDir.mkdirs();
@@ -24,22 +43,11 @@ public class ExportDesignersManager {
             System.out.println("IOException occurred");
         }
 
-        try(FileWriter fw = new FileWriter("src/Designers/designers.csv", true)){
-            for(int i = 0; i < list.size(); i++){
-                if(list.get(i).getDesigner().charAt(0) >= 48  && list.get(i).getDesigner().charAt(0) <= 57){
-                    continue;
-                }
-                if(list.get(i).getDesigner().contains("/")){
-                    String[] designers = list.get(i).getDesigner().split("/");
-                    String result = new String();
-                    for(int j = 0; j < designers.length; j++){
-                        result += designers[j] + ", ";
-                    }
-                    fw.write(result + "\n");
-                }else{
-                    fw.write(list.get(i).getDesigner() + "\n");
-                }
+        List<String> listDesigners = new ArrayList<>(setDesigners);
 
+        try(FileWriter fw = new FileWriter("src/Designers/designers.csv", true)){
+            for(int i = 0; i < listDesigners.size(); i++){
+                fw.write(listDesigners.get(i) + "\n");
             }
         }catch(IOException e){
             System.out.println("IOException occurred");
